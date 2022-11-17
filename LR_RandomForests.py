33fb01888b45dee9918b19d8812b9a8c6b5e8833
Caleb C Sipper <70595437+Darkd143@users.noreply.github.com>
2022-11-17 15:15:43 -0600

    Cleaned up code and improved XGBoost Model

--- LR_RandomForests.py
+++ LR_RandomForests.py
@@ -1,5 +1,3 @@
-# Split the data into training and testing sets
-# Instantiate model with 1000 decision trees
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
@@ -27,43 +25,8 @@ train_x, test_x, train_y, test_y = train_test_split(X, Y,
 
 clf = RandomForestClassifier(n_estimators=100)
 
-# Train the model using the training sets y_pred=clf.predict(X_test)
 clf.fit(train_x, train_y)
 
 predictions = clf.predict(test_x)
 
-# rf = RandomForestClassifier(n_estimators=50)
-# Using Skicit-learn to split data into training and testing sets
-
-# print('Training df Shape:', train_df.shape)
-# print('Training Labels Shape:', train_labels.shape)
-# print('Testing df Shape:', test_df.shape)
-# print('Testing Labels Shape:', test_labels.shape)
-
-# The baseline predictions are the historical averages
-# Baseline errors, and display average baseline error
-# baseline_preds = test_df[:, feature_list.index('average')]
-# baseline_errors = abs(baseline_preds - test_labels)
-
-# print('Average baseline error: ', round(np.mean(baseline_errors), 2))
-
-# Import the model we are using
-# Train the model on training data
-# rf = RandomForestRegressor(n_estimators=1000, random_state=42)
-# rf.fit(X, Y)
-
-# Use the forest's predict method on the test data
-# predictions = rf.predict(test_x)  # Calculate the absolute errors
-# Print out the mean absolute error (mae)
-
-
-results_dict = {'Accuracy': 0, 'Precision': 0, 'Recall': 0}
-############################STUDENT CODE GOES HERE#########################
-# use the decision tree to predict on the test data
-# compare the truth labels with the predicted labels for accuracy, precision, and recall
-# store the results into the dataframe
-results_dict['Accuracy'] = accuracy_score(test_y, predictions)
-# results_dict['Precision'] = precision_score(test_y, predictions)
-# results_dict['Recall'] = recall_score(test_y, predictions)
-
-print(results_dict['Accuracy'])
+print(accuracy_score(test_y, predictions))
--- LR_Xgboost.py
+++ LR_Xgboost.py
@@ -1,20 +1,13 @@
-# Import Packages
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import seaborn as sns
 from sklearn import metrics
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.model_selection import train_test_split
-from numpy import loadtxt
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from sklearn.preprocessing import LabelEncoder
 
 df = pd.read_csv('letter-recognition.csv')
 
-df = pd.DataFrame(df, columns=["letter", "xbox", "ybox", "width", "height", "onpix", "xbar",
+dataframe = pd.DataFrame(df, columns=["letter", "xbox", "ybox", "width", "height", "onpix", "xbar",
                          "ybar", "x2bar", "y2bar", "xybar", "x2ybar", "xy2bar", "xedge", "xedgey", "yedge", "yedgex"])
 
 lab = LabelEncoder()
@@ -24,30 +17,38 @@ df['letter'] = lab.fit_transform(df['letter'])
 X = df.loc[:, df.columns != 'letter'].values
 Y = df["letter"].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.25)
+train_x, test_x, train_y, test_y = train_test_split(X, Y,
+                                                    random_state=42,
+                                                    train_size=0.8,
+                                                    test_size=0.2,
+                                                    shuffle=True)
 
 model = XGBClassifier(silent=False,
                       scale_pos_weight=1,
                       learning_rate=0.01,
                       colsample_bytree=0.4,
                       subsample=0.8,
-                      objective='binary:logistic',
+                      #   objective='binary:logistic',
+                      objective='multi:softprob',
                       n_estimators=1000,
                       reg_alpha=0.3,
                       max_depth=4,
                       gamma=10)
 
-model.fit(X_train, y_train)
+# model = XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+#                       colsample_bynode=1, colsample_bytree=1, gamma=0, learning_rate=0.1,
+#                       max_delta_step=0, max_depth=3, min_child_weight=1, missing=None,
+#                       n_estimators=100, n_jobs=1, nthread=None,
+#                       objective='multi:softprob', random_state=0, reg_alpha=0,
+#                       reg_lambda=1, scale_pos_weight=1, seed=None, silent=None,
+#                       subsample=1, verbosity=1)
 
-expected_y = y_test
-predicted_y = model.predict(X_test)
+model.fit(train_x, train_y)
 
-print(metrics.classification_report(expected_y, predicted_y))
-print(metrics.confusion_matrix(expected_y, predicted_y))
 
-plt.figure(figsize=(10, 10))
-sns.regplot(expected_y, predicted_y, fit_reg=True, scatter_kws={"s": 100})
+predictions = model.predict(test_x)
 
-# X = df.drop('letter',axis=1)
-# y = df['letter']
-# X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=66)
+print(metrics.classification_report(test_y, predictions))
+# print(metrics.confusion_matrix(test_y, predictions))
+
+print(accuracy_score(test_y, predictions))
