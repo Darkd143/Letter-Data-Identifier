e594fc8bbd606e6e14baa9e56ba5c13a141f3e5e
Caleb C Sipper <70595437+Darkd143@users.noreply.github.com>
2022-11-21 10:33:55 -0600

    Updated files

--- LR_RandomForests.py
+++ LR_RandomForests.py
@@ -1,27 +1,33 @@
 import numpy as np
 import pandas as pd
+from sklearn import metrics
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+# from keras.utils import to_categorical
 
-df = pd.read_csv('letter-recognition.csv')
+######## Load Dataset ##########
+path = 'letter-recognition.csv'
+# read .txt file using pandas.
+data = pd.read_csv(path)
+# last 16 colomnn as a inputs
+X = data.iloc[:, 1:]
+# first column as a labels.
+Y = data.iloc[:, 0]
+# total number of classes
+classes = len(np.unique(Y))
+# convert inputs in the range of 0 to 1.
+X = MinMaxScaler().fit_transform(X)
+# convert letter type labels into numeric type( 0 to 25)
+Y = LabelEncoder().fit_transform(Y)
+# # convert labels into one-hot encode respresntation. if label=2 then [0 0 1....0]
+print(Y)
+# Y = to_categorical(Y, classes)
 
-dataframe = pd.DataFrame(df, columns=["letter", "xbox", "ybox", "width", "height", "onpix", "xbar",
-                         "ybar", "x2bar", "y2bar", "xybar", "x2ybar", "xy2bar", "xedge", "xedgey", "yedge", "yedgex"])
 
-lab = LabelEncoder()
-
-df['letter'] = lab.fit_transform(df['letter'])
-
-X = df.loc[:, df.columns != 'letter'].values
-Y = df["letter"].values
-
-train_x, test_x, train_y, test_y = train_test_split(X, Y,
-                                                    random_state=42,
-                                                    train_size=0.8,
-                                                    test_size=0.2,
-                                                    shuffle=True)
+train_x, test_x, train_y, test_y = train_test_split(
+    X, Y, test_size=0.3, random_state=0)
 
 clf = RandomForestClassifier(n_estimators=100)
 
